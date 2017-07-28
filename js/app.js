732e3155c5b1304a56cbb8e1586373e61505ce8c
Vincent Castigliola <vincentcastigliola@Vincents-MacBook-Air.local>
2017-07-28 16:19:00 -0500

    movie

--- js/app.js
+++ js/app.js
@@ -1,1 +1,39 @@
-angular.module('myApp', []);
+angular.module('myApp',['ui.router'])
+    .config(function ($stateProvider, $urlRouterProvider) {
+      $urlRouterProvider.otherwise('/');
+        $stateProvider
+            .state('home',{
+                url:'/home',
+                templateUrl: "../views/home.html"
+            })
+            .state('action',{
+                url:'/action',
+
+                templateUrl: "../views/action.html"
+            })
+            .state('comedy',{
+                url:'/comedy',
+                templateUrl: "../views/comedy.html"
+
+            })
+            .state('drama',{
+                url:'/drama',
+                templateUrl: "../views/drama.html"
+            })
+            .state('romance',{
+                url:'/romance',
+                templateUrl: "../views/romance.html"
+
+            })
+            .state('fantasy',{
+                url:'/fantasy',
+                templateUrl: "../views/fantasy.html"
+            })
+            .state('thriller',{
+                url:'/thriller',
+                templateUrl: "../views/thriller.html"
+            })
+            ;
+
+
+    });
--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,9 +1,20 @@
 angular.module('myApp').controller('mainCtrl', function($scope, mySrvc){
 
+  $scope.index='http://s2.quickmeme.com/img/72/725fe4606d92d9703efef5c820e0131380fc04dcfaa2407b8b7968f8738b5d67.jpg'
+  $scope.tweet = mySrvc.tweet
+   $scope.newImage = function(img){
+     $scope.tweet = mySrvc.tweet
+
+    mySrvc.getQuotes(img).then(function(response){
+      $scope.test = response
+
+    })
 
 
-$scope.newImage = mySrvc.newImage
-  console.log($scope.newImage)
+    $scope.audio = mySrvc.audio
+}
+
+
 
 
 
